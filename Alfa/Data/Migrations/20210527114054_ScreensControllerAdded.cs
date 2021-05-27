4f93c64dadd3808801adcab0e9d097dda0eb9310
geparez <jordan.anastasov@gmail.com>
2021-05-27 14:06:04 +0200

    Screens controller added

---
+++ Alfa/Data/Migrations/20210527114054_ScreensControllerAdded.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Alfa.Data.Migrations
+{
+    public partial class ScreensControllerAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Screen",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Brand = table.Column<string>(maxLength: 15, nullable: false),
+                    Model = table.Column<string>(maxLength: 20, nullable: false),
+                    SerialNumber = table.Column<string>(maxLength: 12, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Screen", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Screen");
+        }
+    }
+}
